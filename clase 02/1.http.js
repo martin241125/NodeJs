26ea25cec9782d701b492850d27ff6a951f5cce8
martin241125 <martincitooo21@gmail.com>
2023-09-25 17:56:03 -0300

    estudiando

--- clase 02/1.http.js
+++ clase 02/1.http.js
@@ -2,10 +2,19 @@ const http = require('node:http') // protocolo HTTP
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
-  console.log('request received')
-  res.end('Hola Mundo')
-})
+const processRequest = (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.end('<h1>Bienvenido a mi pagina de inicio</h1>')
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.end('<h1>Bienvenido a la pagina de contacto</h1> <h1>Bienvenido a la pagina d</h1>')
+  }
+}
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
   console.log(`server listening on port http://localhost:${desiredPort}`)
