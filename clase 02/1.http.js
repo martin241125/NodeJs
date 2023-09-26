28d5e39f29f52f6bbb731725b7cbc55676102188
martin241125 <martincitooo21@gmail.com>
2023-09-26 15:05:14 -0300

    agregando imagen

--- clase 02/1.http.js
+++ clase 02/1.http.js
@@ -1,16 +1,30 @@
 const http = require('node:http') // protocolo HTTP
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
   if (req.url === '/') {
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
     res.end('<h1>Bienvenido a mi pagina de inicio</h1>')
   } else if (req.url === '/contacto') {
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
     res.end('<h1>Bienvenido a la pagina de contacto</h1> <h1>Bienvenido a la pagina d</h1>')
+  } else if (req.url === '/imagen-saga.jpg') {
+    fs.readFile('./saga.jpg', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>Error 500 Internal Server Error </h1>')
+      } else {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'image/jpg; ')
+        res.end(data)
+      }
+    })
   }
 }
 
