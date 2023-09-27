d78ed64f887472bb97472aafab33b3c7f009655c
martin241125 <martincitooo21@gmail.com>
2023-09-27 11:07:47 -0300

    agregando post

--- clase 02/2.routing.js
+++ clase 02/2.routing.js
@@ -1,7 +1,27 @@
 const http = require('node:http')
+const dittoJSON = require('./pokemon/ditto.json')
 
 const processRequest = (req, res) => {
+  const { method, url } = req
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>404 NOT FOUND</h1>')
+      }
 
+    case 'POST':
+      switch (url) {
+        case '/pokemon':{
+          let body = ''
+        }
+      }
+  }
 }
 
 const server = http.createServer(processRequest)
