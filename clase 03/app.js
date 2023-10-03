c856f64fe5726e0a00b4e58d825c08b7ab5a568c
martin241125 <martincitooo21@gmail.com>
2023-10-03 14:09:12 -0300

    agregando querys

--- clase 03/app.js
+++ clase 03/app.js
@@ -6,17 +6,43 @@ app.disable('x-powered-by')
 
 
 app.get('/movies', (req, res) => {
+    const { rate } = req.query
+    if ( rate ) {
+        const filteredRate = movies.filter(
+            movie => movie.rate 
+
+        )
+        return res.json({
+            msg: 'hola',
+            filteredRate
+        })
+    }
     res.json(movies)
 })
 
+app.get('/movies', (req, res) => {
+    const { genre } = req.query
+    if ( genre ) {
+        const filteredMovies = movies.filter(
+            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+            )
+            return res.json(filteredMovies)
+        }
+        res.json(movies)
+    })
+    
+    app.get('/movies/:id', (req, res) => {
+        const { id } = req.params
+        const movie = movies.find(movie => movie.id === id)
+        if(movie) return res.json(movie)
+        
+        res.status(404).json({mesage: '404 NOT FOUND'})
+    })
+    
+    app.get('/movies', (req, res) => {
+        res.json(movies)
+    })
 
-app.get('/movies/:id', (req, res) => {
-    const { id } = req.params
-    const movie = movies.find(movie => movie.id === id)
-    if(movie) return res.json(movie)
-
-    res.status(404).json({mesage: '404 NOT FOUND '})
-})
 
 
 const PORT = process.env.PORT ?? 1234
