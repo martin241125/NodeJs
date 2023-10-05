d7882821e3a39dc2cb86a60492f1048b34a0b52d
martin241125 <martincitooo21@gmail.com>
2023-10-05 18:45:58 -0300

    agregando validaciones

--- clase 03/app.js
+++ clase 03/app.js
@@ -43,6 +43,26 @@ app.get('/movies', (req, res) => {
     })
     
     app.post('/movies', (req, res) => {
+        const movieSchema = z.object({
+            title: z.string({
+                invalid_type_error:'Movie title must be a string.',
+                required_error: 'Movie title is required.'
+            }),
+            year: z.number().int().min(1900).max(2024),
+            director: z.string(),
+            duration: z.number().int().positive(),
+            rate: z.number().min(0).max(10),
+            poster: z.string().url({
+                message: 'Poster must be a valid URL'
+            }),
+            genre: z.array(
+                z.enum(['Action', 'Adventure', 'Comedy', 'Fantasy', 'Drama', 'Horror', 'Thriller', 'Sci-Fi']),
+                {
+                    required_error: 'Movies genre is required.',
+                    invalid_type_error: 'Movie genre must be an array of enum Genre'
+                }
+            )
+        })
         const {
             title,
             genre,
