2f9d8fab395a3581951b5f868ce4425aa05237ce
martin241125 <martincitooo21@gmail.com>
2023-10-06 07:37:19 -0300

    agregando validaciones

--- clase 03/app.js
+++ clase 03/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const movies = require('./movies.json')
 const crypto = require('node:crypto')
 const z = require('zod')
+const validateMovie = require('./schema/movies.js')
 
 const app = express()
 app.disable('x-powered-by')
@@ -43,45 +44,15 @@ app.get('/movies', (req, res) => {
     })
     
     app.post('/movies', (req, res) => {
-        const movieSchema = z.object({
-            title: z.string({
-                invalid_type_error:'Movie title must be a string.',
-                required_error: 'Movie title is required.'
-            }),
-            year: z.number().int().min(1900).max(2024),
-            director: z.string(),
-            duration: z.number().int().positive(),
-            rate: z.number().min(0).max(10),
-            poster: z.string().url({
-                message: 'Poster must be a valid URL'
-            }),
-            genre: z.array(
-                z.enum(['Action', 'Adventure', 'Comedy', 'Fantasy', 'Drama', 'Horror', 'Thriller', 'Sci-Fi']),
-                {
-                    required_error: 'Movies genre is required.',
-                    invalid_type_error: 'Movie genre must be an array of enum Genre'
-                }
-            )
-        })
-        const {
-            title,
-            genre,
-            director,
-            year,
-            duration,
-            rate,
-            poster
-        } = req.body
+       const result = validateMovie(req.body)
+
+       if (result.error) {
+        return res.status(400).json({ error: result.error.message })
+       }
 
         const newMovie = {
             id : crypto.randomUUID(), 
-            title,
-            genre,
-            director,
-            year,
-            duration,
-            rate : rate ?? 0,
-            poster
+            ...result.data
         }
 
         
---
+++ clase 03/schema/movies.js
@@ -0,0 +1,28 @@
+const movieSchema = z.object({
+    title: z.string({
+        invalid_type_error:'Movie title must be a string.',
+        required_error: 'Movie title is required.'
+    }),
+    year: z.number().int().min(1900).max(2024),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    rate: z.number().min(0).max(10).dafault(5.5),
+    poster: z.string().url({
+        message: 'Poster must be a valid URL'
+    }),
+    genre: z.array(
+        z.enum(['Action', 'Adventure', 'Comedy', 'Fantasy', 'Drama', 'Horror', 'Thriller', 'Sci-Fi']),
+        {
+            required_error: 'Movies genre is required.',
+            invalid_type_error: 'Movie genre must be an array of enum Genre'
+        }
+    )
+})
+
+function validateMovie (object) {
+    return movieSchema.safeParse(object)
+}
+
+module.exports = {
+    validateMovie
+}
