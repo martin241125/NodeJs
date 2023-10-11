7c5da46ca29661fe3be768a3601fac0954f18670
martin241125 <martincitooo21@gmail.com>
2023-10-11 07:28:35 -0300

    agregando patch

--- clase 03/app.js
+++ clase 03/app.js
@@ -3,26 +3,13 @@ const movies = require('./movies.json')
 const crypto = require('node:crypto')
 const z = require('zod')
 const validateMovie = require('./schema/movies.js')
-
+const validatePartialMovie = require('./schema/movies.js')
+ 
 const app = express()
 app.disable('x-powered-by')
 app.use(express.json())
 
 
-app.get('/movies', (req, res) => {
-    const { rate } = req.query
-    if ( rate ) {
-        const filteredRate = movies.filter(
-            movie => movie.rate 
-
-        )
-        return res.json({
-            msg: 'Elemento Filtrados',
-            filteredRate
-        })
-    }
-    res.json(movies)
-})
 
 app.get('/movies', (req, res) => {
     const { genre } = req.query
@@ -59,6 +46,32 @@ app.get('/movies', (req, res) => {
         movies.push(newMovie)
 
         res.status(201).json(newMovie) // actualizar la cache del cliente
+    })
+
+    app.patch('/movies/:id', (req, res) => {
+        const result = validatePartialMovie(req.body)
+        
+        if(!result.success) {
+            return res.status(400).json({ error: JSON.parse(result.error.message)})  
+        }
+
+        const { id } = req.params
+        const movieIndex = movies.findIndex(movie => movie.id === id)
+
+        if ( movieIndex === -1) {
+            return res.status(404).json({
+                message: 'Movie not found'
+            })
+        }
+
+        const updateMovie = {
+            ...movies[movieIndex],
+            ...result.data
+        }
+
+        movies[movieIndex] = updateMovie
+
+        return res.json(updateMovie)
     })
 
 
